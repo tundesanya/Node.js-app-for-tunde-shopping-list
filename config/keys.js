555482b123ee08030a61d7ae773f5b9fc86e157c
tunde <tunde02@yahoo.com>
2018-12-07 21:29:30 -0800

    Version 1.0.0

---
+++ config/keys.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mongoURI:
+    // process.env.MONGODB_URI ||
+    // process.env.MONGO_HOST ||
+    // "mongodb://" +
+    //   (process.env.IP || "localhost") +
+    //   ":" +
+    //   (process.env.MONGO_PORT || "27017") +
+    //   "/shop"
+
+    "mongodb://tundesanya:tunde188256@ds125453.mlab.com:25453/fccnodeprojects"
+};
---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const items = require("./routes/api/items");
+
+const app = express();
+
+//Bodyparser middleware
+app.use(bodyParser.json());
+
+//DB config
+const db = require("./config/keys").mongoURI;
+
+//connect to mongo
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB connected..."))
+  .catch(err => console.log(err));
+
+app.use("/api/items", items);
+
+//Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
